7b7b8d34759bd0f10d651b779d2b14fdb7e3dd4d
Andrés <andres.maneiro@automattic.com>
2018-07-13 13:24:04 +0200

    Add allowedBlocks to BlockSettingsMenuPluginsItem
    allowedBlocks is an array that, if present, would make the BlockSettingsMenuPluginsItem
    to check whether the current block type is one of the allowed blocks the plugin
    wants its item to be shown.

--- editor/components/block-settings-menu/block-settings-menu-plugins-group.js
+++ editor/components/block-settings-menu/block-settings-menu-plugins-group.js
@@ -8,11 +8,12 @@ import { isEmpty } from 'lodash';
  */
 import { createSlotFill } from '@wordpress/components';
 import { Fragment } from '@wordpress/element';
+import { withSelect } from '@wordpress/data';
 
 const { Fill: BlockSettingsMenuPluginsGroup, Slot } = createSlotFill( 'BlockSettingsMenuPluginsGroup' );
 
-BlockSettingsMenuPluginsGroup.Slot = ( { fillProps } ) => (
-	<Slot fillProps={ fillProps } >
+const BlockSettingsMenuPluginsGroupSlot = ( { fillProps, currentBlockName } ) => (
+	<Slot fillProps={ { ...fillProps, currentBlockName } } >
 		{ ( fills ) => ! isEmpty( fills ) && (
 			<Fragment>
 				<div className="editor-block-settings-menu__separator" />
@@ -21,5 +22,9 @@ BlockSettingsMenuPluginsGroup.Slot = ( { fillProps } ) => (
 		) }
 	</Slot>
 );
+
+BlockSettingsMenuPluginsGroup.Slot = withSelect( ( select, { fillProps: { uids } } ) => ( {
+	currentBlockName: select( 'core/editor' ).getBlockName( uids ),
+} ) )( BlockSettingsMenuPluginsGroupSlot );
 
 export default BlockSettingsMenuPluginsGroup;
--- editor/components/block-settings-menu/block-settings-menu-plugins-item.js
+++ editor/components/block-settings-menu/block-settings-menu-plugins-item.js
@@ -9,19 +9,26 @@ import { compose } from '@wordpress/element';
  */
 import BlockSettingsMenuPluginsGroup from './block-settings-menu-plugins-group';
 
-const BlockSettingsMenuPluginsItem = ( { icon, label, onClick, small, role } ) => (
+const shouldRenderItem = ( currentBlockName, allowedBlockNames ) => ! Array.isArray( allowedBlockNames ) ||
+	( Array.isArray( allowedBlockNames ) &&
+		allowedBlockNames.some( ( blockName ) => blockName === currentBlockName ) );
+
+const BlockSettingsMenuPluginsItem = ( { allowedBlocks, icon, label, onClick, small, role } ) => (
 	<BlockSettingsMenuPluginsGroup>
-		{ ( fillProps ) => (
-			<IconButton
+		{ ( { currentBlockName, onClose } ) => {
+			if ( ! shouldRenderItem( currentBlockName, allowedBlocks ) ) {
+				return null;
+			}
+			return ( <IconButton
 				className="editor-block-settings-menu__control"
-				onClick={ compose( () => ( onClick( fillProps.firstBlockUID ) ), fillProps.onClose ) }
+				onClick={ compose( onClick, onClose ) }
 				icon={ icon || 'admin-plugins' }
 				label={ small ? label : undefined }
 				role={ role }
 			>
 				{ ! small && label }
-			</IconButton>
-		) }
+			</IconButton> );
+		} }
 	</BlockSettingsMenuPluginsGroup>
 );
 
