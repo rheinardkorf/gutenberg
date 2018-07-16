4ac9cf6aaf006861b283086fdc3c0e83261c3e91
Daniel Richards <daniel.richards@automattic.com>
2018-07-16 16:46:45 +0800

    Revert "Pass component-button__icon class through to icon, and use it for scss selectors"
    This reverts commit 77422fce000a2b75788b5fadaa1cd26e770799e3.
    
    Revert "Add tests for isExternalLink prop in Button component"
    
    This reverts commit 5102826e2c22c7ae59c8d21a3b1fb44781babe5f.
    
    Revert "Use flex to center icon in button and add a tiny bit of margin to adjust"
    
    This reverts commit 0f3c5fa792dc3386e05acd3821ea32b245b42377.
    
    Revert "Set isExternalLink true for post permalink and adjust styling"
    
    This reverts commit 402ecd3ff93f3b945072124f4bd47e15fb3470ce.
    
    Revert "Position dashicon for external links when rendered via Button"
    
    This reverts commit 0930efac6c9eb709b5af7e0e28e9d05d3346bebd.
    
    Revert "Allow Button component to render an ExternalLink"
    
    This reverts commit 1b8e9ca5c5fe67aab5d08d2ffc0798fb7edfaa8d.
    
    Revert "Add prop for icon class name to ExternalLink"
    
    This reverts commit ddac73dd0f09a1076f842fdb89ece370e274ac82.

--- components/external-link/index.js
+++ components/external-link/index.js
@@ -15,24 +15,14 @@ import { __ } from '@wordpress/i18n';
 import Dashicon from '../dashicon';
 import './style.scss';
 
-function ExternalLink( {
-	href,
-	children,
-	className,
-	iconClassName,
-	rel = '',
-	...additionalProps
-} ) {
+function ExternalLink( { href, children, className, rel = '', ...additionalProps } ) {
 	rel = uniq( compact( [
 		...rel.split( ' ' ),
 		'external',
 		'noreferrer',
 		'noopener',
 	] ) ).join( ' ' );
-
 	const classes = classnames( 'components-external-link', className );
-	const iconClasses = classnames( 'components-external-link__icon', iconClassName );
-
 	return (
 		<a { ...additionalProps } className={ classes } href={ href } target="_blank" rel={ rel }>
 			{ children }
@@ -42,7 +32,7 @@ function ExternalLink( {
 					__( '(opens in a new window)' )
 				}
 			</span>
-			<Dashicon className={ iconClasses } icon="external" />
+			<Dashicon icon="external" />
 		</a>
 	);
 }
