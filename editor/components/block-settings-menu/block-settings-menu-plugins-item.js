bada6ccb4d10a3ad43ea8978c6fa1df99e7ee4c4
Andrés <andres.maneiro@automattic.com>
2018-07-16 12:10:59 +0200

    More semantic code
    Improve the logic for when an item should be rendered and add comments.

--- editor/components/block-settings-menu/block-settings-menu-plugins-item.js
+++ editor/components/block-settings-menu/block-settings-menu-plugins-item.js
@@ -14,8 +14,21 @@ import { compose } from '@wordpress/element';
  */
 import BlockSettingsMenuPluginsGroup from './block-settings-menu-plugins-group';
 
+const isEverySelectedBlockAllowed = ( selected, allowed ) => difference( selected, allowed ).length === 0;
+
+/**
+ * Plugins may want to add an item to the menu either for every block
+ * or only for the specific ones provided in the `allowedBlocks` component property.
+ *
+ * If there are multiple blocks selected the item will be rendered if every block
+ * is of one allowed type (not necesarily the same).
+ *
+ * @param {string[]} selectedBlockNames Array containing the names of the blocks selected
+ * @param {string[]} allowedBlockNames Array containing the names of the blocks allowed
+ * @return {boolean} Whether the item will be rendered or not.
+ */
 const shouldRenderItem = ( selectedBlockNames, allowedBlockNames ) => ! Array.isArray( allowedBlockNames ) ||
-	difference( selectedBlockNames, allowedBlockNames ).length === 0;
+	isEverySelectedBlockAllowed( selectedBlockNames, allowedBlockNames );
 
 const BlockSettingsMenuPluginsItem = ( { allowedBlocks, icon, label, onClick, small, role } ) => (
 	<BlockSettingsMenuPluginsGroup>
